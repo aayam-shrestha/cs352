de1dc8713216d8ead80656f8241e61e7b5199cde
Aayam <aayam2071@gmail.com>
2022-02-06 19:39:48 -0500

    added comments

--- proj03/CanvasPaint.js
+++ proj03/CanvasPaint.js
@@ -3,8 +3,8 @@
  * HTML5 canvas element--for CS352, Calvin College Computer Science
  *
  * Begun by: Harry Plantinga -- January 2011
- * Completed by: Aayam Shrestha
- * Date: 02/05/2022
+ * Modified by: Aayam Shrestha
+ * Date: 02/06/2022
  */
 
 $(document).ready(function () {
@@ -23,7 +23,9 @@ var cpaint = {
 
 cpaint.init = function () {
   cpaint.canvas = $("#canvas1")[0];
+  cpaint.sliderCanvas = $("#sliderCanvas")[0];
   cpaint.cx = cpaint.canvas.getContext("2d");
+  cpaint.wx = cpaint.sliderCanvas.getContext("2d");
   cpaint.imgData = cpaint.cx.getImageData(
     0,
     0,
@@ -48,6 +50,10 @@ cpaint.init = function () {
 
   // bind menu options
   $("#menuClear").bind("click", cpaint.clear);
+  $("#menuMarker").bind("click", cpaint.selectMarker);
+  $("#menuLine").bind("click", cpaint.selectLine);
+  $("#menuRect").bind("click", cpaint.selectRect);
+  $("#menuEraser").bind("click", cpaint.erase);
   $("#menuNew").bind("click", cpaint.clear);
   $("#menuFade").bind("click", cpaint.fade);
   $("#menuUnfade").bind("click", cpaint.unfade);
@@ -57,6 +63,20 @@ cpaint.init = function () {
   $("#menuOpen").bind("click", cpaint.open);
   $("#menuSave").bind("click", cpaint.save);
   $("#toolBar").show(); // when toolbar is initialized, make it visible
+
+  // binding individual functions
+  $(".toolbarCell").bind("click", selectTool);
+  $("#menuMarker").bind("click", selectTool);
+  $("#menuLine").bind("click", selectTool);
+  $("#menuRect").bind("click", selectTool);
+  $("#menuEraser").bind("click", selectTool);
+
+  // initializing thickness indicator
+  cpaint.wx.fillStyle = "#333399";
+  cpaint.wx.beginPath();
+  cpaint.wx.arc(21, 14, $("#widthSlider").val() / 2, 0, Math.PI * 2, true);
+  cpaint.wx.closePath();
+  cpaint.wx.fill();
 };
 
 /*
@@ -99,11 +119,6 @@ cpaint.drawEnd = function (ev) {
 
   cpaint.cx.lineWidth = cpaint.lineThickness;
   cpaint.cx.strokeStyle = cpaint.color;
-  // if (cpaint.drawMode == "line") {
-  //   cpaint.cx.moveTo(cpaint.initialX, cpaint.initialY);
-  //   cpaint.cx.lineTo(x, y);
-  //   cpaint.cx.stroke();
-  // }
   cpaint.drawing = false;
   cpaint.cx.beginPath();
 };
@@ -126,7 +141,7 @@ cpaint.drawMarker = function (x, y) {
     cpaint.cx.lineCap = "round";
     cpaint.cx.lineTo(x, y);
     cpaint.cx.stroke();
-    cpaint.cx.beginPath(); // draw initial stroke
+    cpaint.cx.beginPath();
     cpaint.cx.moveTo(x, y);
   }
 };
@@ -179,7 +194,7 @@ cpaint.clear = function (ev) {
  */
 cpaint.selectLine = function (ev) {
   $("#messages").prepend("Line tool selected<br>");
-  cpaint.drawMode = "line";
+  cpaint.drawMode = "line"; // change selected tool to line
   cpaint.colorChange();
 };
 
@@ -188,7 +203,7 @@ cpaint.selectLine = function (ev) {
  */
 cpaint.selectRect = function (ev) {
   $("#messages").prepend("Rectangle tool selected<br>");
-  cpaint.drawMode = "rect";
+  cpaint.drawMode = "rect"; // change selected tool to rectangle
   cpaint.colorChange();
 };
 
@@ -198,11 +213,13 @@ cpaint.selectRect = function (ev) {
 cpaint.colorChange = function (ev) {
   $("#messages").prepend("Color: " + $("#color1").val() + "<br>");
   cpaint.color = $("#color1").val();
+  cpaint.wx.fillStyle = $("#color1").val();
+  cpaint.thicknessChange();
 };
 
 cpaint.selectMarker = function () {
   $("#messages").prepend("Marker selected<br>");
-  cpaint.drawMode = "free";
+  cpaint.drawMode = "free"; // change selected tool to marker
   cpaint.colorChange();
 };
 
@@ -212,6 +229,18 @@ cpaint.selectMarker = function () {
 cpaint.thicknessChange = function (ev) {
   $("#messages").prepend("Thickness: " + $("#widthSlider").val() + "<br>");
   cpaint.lineThickness = $("#widthSlider").val();
+
+  // Redrawing thickness indicator with corresponding radius
+  cpaint.wx.clearRect(
+    0,
+    0,
+    cpaint.sliderCanvas.width,
+    cpaint.sliderCanvas.height
+  );
+  cpaint.wx.beginPath();
+  cpaint.wx.arc(21, 14, $("#widthSlider").val() / 2, 0, Math.PI * 2, true);
+  cpaint.wx.closePath();
+  cpaint.wx.fill();
 };
 
 /*
@@ -219,7 +248,7 @@ cpaint.thicknessChange = function (ev) {
  */
 cpaint.erase = function (ev) {
   $("#messages").prepend("Erasing<br>");
-  cpaint.color = $("#canvas1").css("backgroundColor");
+  cpaint.color = $("#canvas1").css("backgroundColor"); // changing brush color to background color
   cpaint.drawMode = "free";
 };
 
@@ -312,6 +341,9 @@ cpaint.unfade = function (ev) {
   cpaint.cx.putImageData(cpaint.imgData, 0, 0);
 };
 
+/*
+ * Blur an image by applying a blurring kernel (averaging the color values of neighboring pixels)
+ */
 cpaint.blur = function (ev) {
   $("#messages").prepend("Blur<br>");
   cpaint.imgData = cpaint.cx.getImageData(
@@ -320,10 +352,11 @@ cpaint.blur = function (ev) {
     cpaint.canvas.width,
     cpaint.canvas.height
   );
-  cpaint.blur = new ImageData(cpaint.canvas.width, cpaint.canvas.height);
+  cpaint.blur = new ImageData(cpaint.canvas.width, cpaint.canvas.height); // creating a new image to store the blurred image
 
   var pix = cpaint.imgData.data;
   var blurMatrix = [
+    // Matrix used for blurring
     [1 / 9, 1 / 9, 1 / 9],
     [1 / 9, 1 / 9, 1 / 9],
     [1 / 9, 1 / 9, 1 / 9],
@@ -335,11 +368,15 @@ cpaint.blur = function (ev) {
 
   for (var row = 0; row < cpaint.canvas.height; row += 1) {
     for (var col = 0; col < cpaint.canvas.width; col += 1) {
+      //converting the row and column values to a corresponding index in the 1d array of pixels
       var destNum = 4 * row * cpaint.canvas.width + col * 4;
+
+      // Loop to multiply the 3x3 grid of pixels surrounding each pixel with the corresponding value in the kernel
       for (var y = 0; y < 3; y += 1) {
         for (var x = 0; x < 3; x += 1) {
           var sourceNum =
             4 * (row - 1 + y) * cpaint.canvas.width + (col - 1 + x) * 4;
+          // Convolving with the blurring matrix
           red += pix[sourceNum] * blurMatrix[x][y];
           green += pix[sourceNum + 1] * blurMatrix[x][y];
           blue += pix[sourceNum + 2] * blurMatrix[x][y];
@@ -383,11 +420,15 @@ cpaint.sharpen = function (ev) {
 
   for (var row = 0; row < cpaint.canvas.height; row += 1) {
     for (var col = 0; col < cpaint.canvas.width; col += 1) {
+      //converting the row and column values to a corresponding index in the 1d array of pixels
       var destNum = 4 * row * cpaint.canvas.width + col * 4;
+
+      // Loop to multiply the 3x3 grid of pixels surrounding each pixel with the corresponding value in the kernel
       for (var y = 0; y < 3; y += 1) {
         for (var x = 0; x < 3; x += 1) {
           var sourceNum =
             4 * (row - 1 + y) * cpaint.canvas.width + (col - 1 + x) * 4;
+          // Convolving with the sharpening matrix
           red += pix[sourceNum] * sharpenMatrix[x][y];
           green += pix[sourceNum + 1] * sharpenMatrix[x][y];
           blue += pix[sourceNum + 2] * sharpenMatrix[x][y];
@@ -416,9 +457,12 @@ cpaint.detectEdges = function (ev) {
     cpaint.canvas.width,
     cpaint.canvas.height
   );
+  // Creating new image
   cpaint.edges = new ImageData(cpaint.canvas.width, cpaint.canvas.height);
+
   var pix = cpaint.imgData.data;
 
+  // Sobel kernels for detecting edges along the x and y axis
   var sobelX = [
     [-1, 0, 1],
     [-2, 0, 2],
@@ -431,53 +475,69 @@ cpaint.detectEdges = function (ev) {
     [-1, -2, -1],
   ];
 
+  // Store color values for each pixel after convolving with sobelX kernel
   var redX = 0,
     greenX = 0,
     blueX = 0,
     alphaX = 0;
+
+  // Store color values for each pixel after convolving with sobelY kernel
   var redY = 0,
     greenY = 0,
     blueY = 0,
     alphaY = 0;
+
+  // Store color values for each pixel after combining the color values obtianed from both kernels
   var finalRed = 0,
     finalGreen = 0,
     finalBlue = 0,
     finalAlpha = 0;
+
   for (var row = 0; row < cpaint.canvas.height; row += 1) {
     for (var col = 0; col < cpaint.canvas.width; col += 1) {
+      //converting the row and column values to a corresponding index in the 1d array of pixels
       var destNum = 4 * row * cpaint.canvas.width + col * 4;
+
+      // Loop to multiply the 3x3 grid of pixels surrounding each pixel with the corresponding value in the kernel
       for (var y = 0; y < 3; y += 1) {
         for (var x = 0; x < 3; x += 1) {
           var sourceNum =
             4 * (row - 1 + y) * cpaint.canvas.width + (col - 1 + x) * 4;
+
+          // For horizontal edges
           redX += pix[sourceNum] * sobelX[x][y];
           greenX += pix[sourceNum + 1] * sobelX[x][y];
           blueX += pix[sourceNum + 2] * sobelX[x][y];
           alphaX += pix[sourceNum + 3] * sobelX[x][y];
 
+          // For vertical edges
           redY += pix[sourceNum] * sobelY[x][y];
           greenY += pix[sourceNum + 1] * sobelY[x][y];
           blueY += pix[sourceNum + 2] * sobelY[x][y];
           alphaY += pix[sourceNum + 3] * sobelY[x][y];
         }
       }
+      // Combining the result of the sobelX and sobelY operations(square root of sum of squares)
       finalRed = Math.sqrt(redX ** 2 + redY ** 2);
       finalGreen = Math.sqrt(greenX ** 2 + greenY ** 2);
       finalBlue = Math.sqrt(blueX ** 2 + blueY ** 2);
       finalAlpha = Math.sqrt(alphaX ** 2 + alphaY ** 2);
 
+      // If the average of the colors is above a threshold, color the pixel white
       if ((finalRed + finalGreen + finalBlue) / 3 > 80) {
         cpaint.edges.data[destNum] = 255;
         cpaint.edges.data[destNum + 1] = 255;
         cpaint.edges.data[destNum + 2] = 255;
         cpaint.edges.data[destNum + 3] = 255;
       } else {
+        // Otherwise color it black
         cpaint.edges.data[destNum] = 0;
         cpaint.edges.data[destNum + 1] = 0;
         cpaint.edges.data[destNum + 2] = 0;
         cpaint.edges.data[destNum + 3] = 255;
       }
 
+      // Resetting the variables
       redX = 0;
       redY = 0;
       blueX = 0;
@@ -490,4 +550,27 @@ cpaint.detectEdges = function (ev) {
   }
 
   cpaint.cx.putImageData(cpaint.edges, 0, 0);
+};
+
+/*
+ * Visual state handler for toolbar buttons
+ */
+selectTool = function (ev) {
+  $(".toolbarCell").each(function (index) {
+    if (ev.target.id != "clearButton" && ev.target.id != "width") {
+      if ($(this).hasClass("selected")) {
+        $(this).removeClass("selected");
+      }
+      console.log(ev.target.id);
+      //Special selector for toolbar cells
+      if (ev.target.id.substring(0, 4) == "menu") {
+        let toolID = "#" + ev.target.id.substring(4).toLowerCase() + "Button";
+        console.log(toolID);
+        $(toolID).addClass("selected");
+      } else {
+        //General selector for GUI
+        $("#" + ev.target.id).addClass("selected");
+      }
+    }
+  });
 };
